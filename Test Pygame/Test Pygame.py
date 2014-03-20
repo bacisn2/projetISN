ee802239485602fe09f9d205d33eb80cc3e91ee2
ValentinCarpentier <valentin.carpentier@gmail.com>
2014-03-20 22:17:13 +0100

    Nouveau Test Pygame + Images Bateaux
    Le programme peut afficher des bateaux quand on appuie sur les touches
    du clavier (2, 3, 4 ou 5), réinitialise le plateau quand on apuie sur
    "haut". Le plateau a été revue (20*20 cases) et des images de bateaux
    sont incluses (la majorité ne sont pas encore utilisé).

--- Test Pygame/Test Pygame.py
+++ Test Pygame/Test Pygame.py
@@ -9,48 +9,72 @@ import pygame
 from pygame.locals import *
 pygame.init()
 pygame.font.init()
-font = pygame.font.Font(None, 30)
-SuperFont = pygame.font.Font(None, 80)
+font = pygame.font.Font(None, 30) #Transparent pour l'afiichage des Textes (version 30 px)
+SuperFont = pygame.font.Font(None, 80) #Transparent pour l'afiichage des Textes (version 80 px)
 
-fenetre = pygame.display.set_mode((800, 500))
-fond = pygame.image.load("Fond_Blanc.png")
+#Ini Fenêtre + Fond Blanc
+fenetre = pygame.display.set_mode((1200, 900))
+fond = pygame.image.load("Fond_Blanc_1200-900.jpg")
 fenetre.blit(fond, (0,0))
 pygame.display.flip()
 
-Quad1 = pygame.image.load("quadrillage 3.png")
-fenetre.blit(Quad1, (50,50))
+#Ini Quadriallage (sans marquage)
+Quad1 = pygame.image.load("The Quadrillage 2.jpg")
+fenetre.blit(Quad1, (100,100))
+fenetre.blit(Quad1, (600,100))
 pygame.display.flip()
 
-Quad2 = pygame.image.load("quadrillage 3.png")
-fenetre.blit(Quad2, (450,50))
+#Ini marquage Quadrillage
+Ligne = pygame.image.load("Ligne_Tableau.jpg")
+Colonne = pygame.image.load("Colonne_Tableau.jpg")
+fenetre.blit(Ligne, (80,100))
+fenetre.blit(Ligne, (580,100))
+fenetre.blit(Colonne, (100,75))
+fenetre.blit(Colonne, (600,75))
 pygame.display.flip()
 
-Infinie = 1
+Infinie = 1 #Fait tourner indéfiniment le programme
 
 while Infinie == 1:
-    for event in pygame.event.get():
-        if event.type == QUIT:
+    for event in pygame.event.get(): #Fait la liste des évènements possible (appuyer sur une touche, souris, etc...)
+        if event.type == QUIT: #Quitte la partie quand on appuie sur "Quitter"
             Infinie = 0
-    if event.type == KEYDOWN and event.key == K_SPACE:
-        Obj = "Espace"
-        Esp = font.render(Obj, 1, (0,0,0))
-        fenetre.blit(Esp, (100,400))
-        pygame.display.flip()
-    if event.type == KEYDOWN and event.key == K_LEFT:
-        Bat = pygame.image.load("bateau.jpeg").convert_alpha()
-        fenetre.blit(Bat, (300,150))
-        pygame.display.flip()
-    if event.type == KEYDOWN and event.key == K_RIGHT:
-        fenetre.blit(fond, (0,0))
-        fenetre.blit(Quad1, (50,50))
-        fenetre.blit(Quad2, (450,50))
-        pygame.display.flip()
-    if event.type == KEYDOWN and event.key == K_ESCAPE:
-        Obj2 = "N'essayez pas de vous echapper..."
-        Obj3 = "Vous etes prisonnier !!"
-        Esp2 = font.render(Obj2, 1, (255,0,0))
-        Esp3 = font.render(Obj3, 1, (255,0,0))
-        fenetre.blit(Esp2, (400,400))
-        fenetre.blit(Esp3, (400,425))
-        pygame.display.flip()
-
+            break
+        if event.type == KEYDOWN and event.key == K_SPACE: #Test Espace
+            Obj = "Espace"
+            Esp = font.render(Obj, 1, (0,0,0))
+            fenetre.blit(Esp, (200,500))
+            pygame.display.flip()
+        if event.type == KEYDOWN and event.key == K_ESCAPE: #Message Mystère
+            Obj2 = "N'essayez pas de vous echapper..."
+            Obj3 = "Vous etes prisonnier !!"
+            Esp2 = font.render(Obj2, 1, (255,0,0))
+            Esp3 = font.render(Obj3, 1, (255,0,0))
+            fenetre.blit(Esp2, (800,500))
+            fenetre.blit(Esp3, (800,525))
+            pygame.display.flip()            
+        if event.type == KEYDOWN and event.key == K_KP5: #Placement bateau 5 cases (K6)
+            Bat = pygame.image.load("bateau 23.jpg").convert_alpha()
+            fenetre.blit(Bat, (205,305))
+            pygame.display.flip()
+        if event.type == KEYDOWN and event.key == K_KP2: #Placement bateau 2 cases (T19)
+            Bat = pygame.image.load("bateau 10.jpg").convert_alpha()
+            fenetre.blit(Bat, (465,485))
+            pygame.display.flip()            
+        if event.type == KEYDOWN and event.key == K_KP4: #Placement du bateau 4 cases (A1)
+            Bat = pygame.image.load("bateau 22.jpg").convert_alpha()
+            fenetre.blit(Bat, (105,105))
+            pygame.display.flip()
+        if event.type == KEYDOWN and event.key == K_KP3: #Placement du bateau 3 cases (J15)
+            Bat = pygame.image.load("Bateau 4 (sous-marin 1) V.jpg").convert_alpha()
+            fenetre.blit(Bat, (385,285))
+            pygame.display.flip()            
+        if event.type == KEYDOWN and event.key == K_UP: #réinitialisation du plateau
+            fenetre.blit(fond, (0,0))
+            fenetre.blit(Quad1, (100,100))
+            fenetre.blit(Quad1, (600,100))
+            fenetre.blit(Ligne, (80,100))
+            fenetre.blit(Ligne, (580,100))
+            fenetre.blit(Colonne, (100,75))
+            fenetre.blit(Colonne, (600,75))
+            pygame.display.flip()
