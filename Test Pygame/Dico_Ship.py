2b48d7b36a61f280ef7042c6004c9ed587ddcc98
ValentinCarpentier <valentin.carpentier@gmail.com>
2014-03-22 14:42:01 +0100

    dernière modif
    Le programme a été optimisé en terme de poids. Il appelle désormais ses
    immenses bibliothèques de cases depuis un autre programme (il est donc
    allégé)

---
+++ Test Pygame/Dico_Ship.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 22 14:23:50 2014
+
+@author: IG 158
+"""
+
+#Initialisation Pygame (pour le pygame.image.load)
+import pygame
+from pygame.locals import *
+pygame.init()
+pygame.font.init()
+fenetre = pygame.display.set_mode((1200, 900)) #obligé d'initialiser une fenêtre pour faire fonctionner le pygame.image.load
+
+#Dico Liste Bateaux (for Player 1)
+Ship1 = {"LitShip":pygame.image.load("bateau 10.jpg").convert_alpha(), "LitShipV":pygame.image.load("bateau 10 V.jpg").convert_alpha(),"SMV":pygame.image.load("sous-marin 1 V.jpg").convert_alpha(), "SM":pygame.image.load("sous-marin 1.jpg").convert_alpha(), "CuirBase":pygame.image.load("bateau 22.jpg").convert_alpha(), "CuirBaseV":pygame.image.load("bateau 22 V.jpg").convert_alpha(), "CuirSuper":pygame.image.load("bateau 25.jpg").convert_alpha(), "CuirSuperV":pygame.image.load("bateau 25 V.jpg").convert_alpha(), "Cuir2":pygame.image.load("bateau 26.jpg").convert_alpha(), "Cuir2V":pygame.image.load("bateau 26 V.jpg").convert_alpha(), "PA":pygame.image.load("bateau 23.jpg").convert_alpha(), "PAV":pygame.image.load("bateau 23 V.jpg").convert_alpha()}
+
+#Dico Liste Bateaux (for IA)
