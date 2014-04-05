350492c591f1f21ca1b46a0372b9147c3d5f9bca
mickael-parlange <mickael.parlange@gmail.com>
2014-04-05 16:55:17 +0200

    Début IA pose bateaux

---
+++ Jeu Bataille Navale/IA_pose_bat.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 05 16:18:58 2014
+
+@author: mickael
+"""
+
+import random
+import Dico_Grille2
+CasePlayer2 = Dico_Grille2.GrillePlayer2
+a = random.randrange(0,2)
+if a ==0 :
+    
