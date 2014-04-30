c51abf123216637a8b88ae6493e463cb71eed22d
mickael-parlange <mickael.parlange@gmail.com>
2014-04-30 17:18:26 +0200

    Fonction Coulage commentée

--- Jeu Bataille Navale/Coulage.py
+++ Jeu Bataille Navale/Coulage.py
@@ -7,12 +7,15 @@ Created on Thu Apr 17 20:37:51 2014
 
 def Coulage(Grille2, IndexCible, NbCoul) :
     from Fonction_Ship import Grille2
-    liste = []
-    x = 0
-    z = 0
-    if Grille2[IndexCible-1][1] == 1 :
-        a = IndexCible-1
-        liste.append(a)
+    liste = [] #Liste des index des cases qui vont devoir être vérifiées (touché ou non)
+    x = 0 #Variable pour le parcourt de la liste des index
+    z = 0 #Variable du nombre de case avec bateau qui sont touchées
+    #Début des vérifications case par case si il y a un bateau ou non
+    #De un en un pour les cases horizontales
+    #De vingt en vingt pour les cases verticales
+    if Grille2[IndexCible-1][1] == 1 : #Vérification si il y a un bateaux
+        a = IndexCible-1 #Variable de l'index de la case
+        liste.append(a) #Rajout de cette variable dans la liste des index
         if Grille2[IndexCible-2][1] == 1 :
             b = IndexCible-2
             liste.append(b)
@@ -70,14 +73,15 @@ def Coulage(Grille2, IndexCible, NbCoul) :
                     if Grille2[IndexCible+100][1] == 1 :
                         e = IndexCible+100
                         liste.append(e)
-    print "liste index coulage :"
-    print liste     
-    while x < len(liste) :
-        if Grille2[liste[x]][2] == 1 :
-            z = z + 1
-        x = x + 1
-    if x == z :
-        print "Coulé"
-        print "NbCoul avant rajout :" + str(NbCoul)
+    #Début des vérifications case par case si il y a un bateau ou non
+    print "liste index coulage :" #print pour test
+    print liste #print pour test
+    while x < len(liste) : #Boucle avec incrémentation de x jusqu'au nombre d'élément dans la liste
+        if Grille2[liste[x]][2] == 1 : #Vérification si la case a été touchée ou non
+            z = z + 1 #Incrémentation de la variable du nombre de case avec bateau qui sont touchées
+        x = x + 1 #Incrémentation de x pour le parcourt de la boucle
+    if x == z : #Si le nombre de case avec bateau qui sont touchée est égal au nombre de case devant être vérifiée
+        print "Coulé" #print pour test
+        print "NbCoul avant rajout :" + str(NbCoul) #print pour test
         NbCoul = NbCoul + 1
         print "Le nombre de bateau coulé est de " + str(NbCoul)
