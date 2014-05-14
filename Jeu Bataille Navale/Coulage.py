2a95ff4a84c9c69819c57e1e419ba298aae1fb52
mickael-parlange <mickael.parlange@gmail.com>
2014-05-14 16:03:17 +0200

    Correction fonction Coulage
    La fonction Coulage vérifie maintenant la position des cases avant de
    tester.

--- Jeu Bataille Navale/Coulage.py
+++ Jeu Bataille Navale/Coulage.py
@@ -14,66 +14,87 @@ def Coulage(Grille2, IndexCible):#, NbCoulInterm) :
     #Début des vérifications case par case si il y a un bateau ou non
     #De un en un pour les cases horizontales
     #De vingt en vingt pour les cases verticales
-    if Grille2[IndexCible-1][1] == 1 : #Vérification si il y a un bateaux
-        a = IndexCible-1 #Variable de l'index de la case
-        liste.append(a) #Rajout de cette variable dans la liste des index
-        if Grille2[IndexCible-2][1] == 1 :
-            b = IndexCible-2
-            liste.append(b)
-            if Grille2[IndexCible-3][1] == 1 :
-                c = IndexCible-3
-                liste.append(c)
-                if Grille2[IndexCible-4][1] == 1 :
-                    d = IndexCible-4
-                    liste.append(d)
-                    if Grille2[IndexCible-5][1] == 1 :
-                        e = IndexCible-5
-                        liste.append(e)
-    if Grille2[IndexCible+1][1] == 1 :
-        a = IndexCible+1
-        liste.append(a)
-        if Grille2[IndexCible+2][1] == 1 :
-            b = IndexCible+2
-            liste.append(b)
-            if Grille2[IndexCible+3][1] == 1 :
-                c = IndexCible+3
-                liste.append(c)
-                if Grille2[IndexCible+4][1] == 1 :
-                    d = IndexCible+4
-                    liste.append(d)
-                    if Grille2[IndexCible+5][1] == 1 :
-                        e = IndexCible+5
-                        liste.append(e)
-    if Grille2[IndexCible-20][1] == 1 :
-        a = IndexCible-20
-        liste.append(a)
-        if Grille2[IndexCible-40][1] == 1 :
-            b = IndexCible-40
-            liste.append(b)
-            if Grille2[IndexCible-60][1] == 1 :
-                c = IndexCible-60
-                liste.append(c)
-                if Grille2[IndexCible-80][1] == 1 :
-                    d = IndexCible-80
-                    liste.append(d)
-                    if Grille2[IndexCible-100][1] == 1 :
-                        e = IndexCible-100
-                        liste.append(e)
-    if Grille2[IndexCible+20][1] == 1 :
-        a = IndexCible+20
-        liste.append(a)
-        if Grille2[IndexCible+40][1] == 1 :
-            b = IndexCible+40
-            liste.append(b)
-            if Grille2[IndexCible+60][1] == 1 :
-                c = IndexCible+60
-                liste.append(c)
-                if Grille2[IndexCible+80][1] == 1 :
-                    d = IndexCible+80
-                    liste.append(d)
-                    if Grille2[IndexCible+100][1] == 1 :
-                        e = IndexCible+100
-                        liste.append(e)
+    CoordCible = list(Grille2[IndexCible][0]) #Transforme les coordonnées de l'IndexCible en liste pour être exploitées
+    if CoordCible[0] != 605 : #Vérifie la colonne de l'IndexCible
+        if Grille2[IndexCible-1][1] == 1 : #Vérification si il y a un bateaux
+            a = IndexCible-1 #Variable de l'index de la case
+            liste.append(a) #Rajout de cette variable dans la liste des index
+            if CoordCible[0] != 625 :
+                if Grille2[IndexCible-2][1] == 1 :
+                    b = IndexCible-2
+                    liste.append(b)
+                    if CoordCible[0] != 645 :
+                        if Grille2[IndexCible-3][1] == 1 :
+                            c = IndexCible-3
+                            liste.append(c)
+                            if CoordCible[0] != 665 :
+                                if Grille2[IndexCible-4][1] == 1 :
+                                    d = IndexCible-4
+                                    liste.append(d)
+                                    if CoordCible[0] != 685 :
+                                        if Grille2[IndexCible-5][1] == 1 :
+                                            e = IndexCible-5
+                                            liste.append(e)
+    if CoordCible[0] != 985 :
+        if Grille2[IndexCible+1][1] == 1 :
+            a = IndexCible+1
+            liste.append(a)
+            if CoordCible[0] != 965 :
+                if Grille2[IndexCible+2][1] == 1 :
+                    b = IndexCible+2
+                    liste.append(b)
+                    if CoordCible[0] != 945 :
+                        if Grille2[IndexCible+3][1] == 1 :
+                            c = IndexCible+3
+                            liste.append(c)
+                            if CoordCible[0] != 925 :
+                                if Grille2[IndexCible+4][1] == 1 :
+                                    d = IndexCible+4
+                                    liste.append(d)
+                                    if CoordCible[0] != 905 :
+                                        if Grille2[IndexCible+5][1] == 1 :
+                                            e = IndexCible+5
+                                            liste.append(e)
+    if CoordCible[1] != 105 : #Teste de la ligne pour CoordCible[1]
+        if Grille2[IndexCible-20][1] == 1 :
+            a = IndexCible-20
+            liste.append(a)
+            if CoordCible[1] != 125 :
+                if Grille2[IndexCible-40][1] == 1 :
+                    b = IndexCible-40
+                    liste.append(b)
+                    if CoordCible[1] != 145 :
+                        if Grille2[IndexCible-60][1] == 1 :
+                            c = IndexCible-60
+                            liste.append(c)
+                            if CoordCible[1] != 165 :
+                                if Grille2[IndexCible-80][1] == 1 :
+                                    d = IndexCible-80
+                                    liste.append(d)
+                                    if CoordCible[1] != 185 :
+                                        if Grille2[IndexCible-100][1] == 1 :
+                                            e = IndexCible-100
+                                            liste.append(e)
+    if CoordCible[1] != 485 :
+        if Grille2[IndexCible+20][1] == 1 :
+            a = IndexCible+20
+            liste.append(a)
+            if CoordCible[1] != 465 :
+                if Grille2[IndexCible+40][1] == 1 :
+                    b = IndexCible+40
+                    liste.append(b)
+                    if CoordCible[1] != 445 :
+                        if Grille2[IndexCible+60][1] == 1 :
+                            c = IndexCible+60
+                            liste.append(c)
+                            if CoordCible[1] != 425 :
+                                if Grille2[IndexCible+80][1] == 1 :
+                                    d = IndexCible+80
+                                    liste.append(d)
+                                    if CoordCible[1] != 405 :
+                                        if Grille2[IndexCible+100][1] == 1 :
+                                            e = IndexCible+100
+                                            liste.append(e)
     #Début des vérifications case par case si il y a un bateau ou non
     print "liste index coulage :" #print pour test
     print liste #print pour test
